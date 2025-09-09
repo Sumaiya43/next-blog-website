809dc51690d767c56176fc396d2b4df2c1a48104
Sumaiya Begum <sumaiyabegum43@gmail.com>
2025-09-09 15:53:45 -0700

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,16 +5,16 @@ import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
-export default function Home({searchParams}) {
-  const page = parseInt(searchParams.page) || 1;
+export default function Home() {
+  // const page = parseInt(searchParams.page) || 1;
   return (
     <div className={styles.container}>
-      <Featured />
+      {/* <Featured />
       <CategoryList />
       <div className={styles.content}>
         <CardList page={page} />
         <Menu />
-      </div>
+      </div> */}
     </div>
   );
  
