0f906b71d5d139a0afd3a10bc540a785529f92a4
Sumaiya Begum <sumaiyabegum43@gmail.com>
2025-09-09 02:10:54 -0700

    updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,17 +5,18 @@ import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
 export default function Home({searchParams}) {
-  const page = parseInt(searchParams.page) || 1;
-  return (
-    <div className={styles.container}>
-      <Featured />
-      <CategoryList />
-      <div className={styles.content}>
-        <CardList page={page} />
-        <Menu />
-      </div>
-    </div>
-  );
+  // const page = parseInt(searchParams.page) || 1;
+  // return (
+  //   <div className={styles.container}>
+  //     <Featured />
+  //     <CategoryList />
+  //     <div className={styles.content}>
+  //       <CardList page={page} />
+  //       <Menu />
+  //     </div>
+  //   </div>
+  // );
+  <div>home is working</div>
 }
 
 
