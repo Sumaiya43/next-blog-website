3da40d00fc112e74bcd0414c160c3d63808ee87b
Sumaiya Begum <sumaiyabegum43@gmail.com>
2025-09-09 16:55:18 -0700

    check test

---
+++ src/app/check/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>test check</div>
+  )
+}
+
+export default page
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,27 +5,26 @@ import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
 
 const LoginPage = () => {
-  // const s = useSession();
-  // const data = s?.data;
-  // const status = s?.status;
+  const s = useSession();
+  const data = s?.data;
+  const status = s?.status;
 
-  // console.log(data, status);
+  console.log(data, status);
 
-  // return (
-  //   <div className={`${styles.loginContainer} ${styles.fullBleed}`}>
-  //     <div className={styles.wrapper}>
-  //       <div
-  //         className={styles.socialButton}
-  //         onClick={() => signIn("google", { callbackUrl: "/" })}
-  //       >
-  //         Sign in with Google
-  //       </div>
-  //       <div className={styles.socialButton}>Sign in with Github</div>
-  //       <div className={styles.socialButton}>Sign in with Facebook</div>
-  //     </div>
-  //   </div>
-  // );
-  <div>hi</div>
+  return (
+    <div className={`${styles.loginContainer} ${styles.fullBleed}`}>
+      <div className={styles.wrapper}>
+        <div
+          className={styles.socialButton}
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+        >
+          Sign in with Google
+        </div>
+        <div className={styles.socialButton}>Sign in with Github</div>
+        <div className={styles.socialButton}>Sign in with Facebook</div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
