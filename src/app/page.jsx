5216b1ef8aecc41609f3d109b4aee7288dcc578f
Sumaiya Begum <sumaiyabegum43@gmail.com>
2025-09-09 17:17:58 -0700

    hoomepage check cardlist

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home({ searchParams }) {
       <Featured />
       <CategoryList />
       <div className={styles.content}>
-        {/* <CardList page={page} /> */}
+        <CardList page={page} />
         <Menu />
       </div>
     </div>
