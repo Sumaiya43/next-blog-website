bfa736fb57f4360b5b5f6154e8ad1175134a8622
Sumaiya Begum <sumaiyabegum43@gmail.com>
2025-09-09 18:21:02 -0700

    hoomepage test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,9 +11,9 @@ export default function Home({ searchParams }) {
   return (
     <div className={styles.container}>
       <Featured />
-      <CategoryList /> 
+      {/* <CategoryList />  */}
       <div className={styles.content}>
-        {/* <CardList page={page} />     */}
+        <CardList page={page} />    
         <Menu />
       </div>
     </div>
